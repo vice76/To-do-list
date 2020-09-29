6e6b9a266748f12b19046a9edcefe41e5e563fd0
vice76 <arpityadavaj1999@gmail.com>
2020-09-29 10:17:13 +0530

    Updates

---
+++ precache-manifest.5dacd900e34c3604abb56c9468d61387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e74283cfa12b105e0d307f275f100a",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "e1a0ded9114e0006f6b0",
+    "url": "/To-do-list/static/css/main.69ebd703.chunk.css"
+  },
+  {
+    "revision": "14b228f7e9b2faff47b6",
+    "url": "/To-do-list/static/js/2.53d65977.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-do-list/static/js/2.53d65977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a0ded9114e0006f6b0",
+    "url": "/To-do-list/static/js/main.0fd607a6.chunk.js"
+  },
+  {
+    "revision": "b243709fc4e18849f9d8",
+    "url": "/To-do-list/static/js/runtime-main.a5b8843b.js"
+  }
+]);
